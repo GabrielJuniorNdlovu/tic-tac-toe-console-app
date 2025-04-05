4e03803688a7441cfbe23454a004654f512d8b50
Theeangelgabriel_ <ndlovujnr18@gmail.com>
2025-04-05 22:31:24 +0200

    fix comment typo and add get_move method placeholder

--- player.py
+++ player.py
@@ -3,5 +3,9 @@ import random
 
 class Player:
     def __init__(self, letter):
-        #letter is xor o
+        #letter is x or o
         self.letter = letter
+
+    # WE WANT ALL PLYERS TO GET THEIR NEXT MOVE
+    def get_move(self, game):
+        pass
